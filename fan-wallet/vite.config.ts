f368bba962a4493f0d33518eef8dab8df28b36cf
Damian Perez <dajpes@gmail.com>
2026-01-07 22:50:52 -0500

    MF-2: Fixed even more lint errors

--- fan-wallet/vite.config.ts
+++ fan-wallet/vite.config.ts
@@ -1,6 +1,6 @@
+import federation from '@originjs/vite-plugin-federation'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig, loadEnv } from 'vite'
-import federation from '@originjs/vite-plugin-federation'
 import biomePlugin from 'vite-plugin-biome'
 
 // https://vite.dev/config/
