fe4a051d1571af00334154baaa4644e9907cde35
Damian Perez <dajpes@gmail.com>
2026-01-08 16:28:00 -0500

    Keep calm and commit

---
+++ host/.lintstagedrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+ "*": (files) => {
+    console.log(files)
+    return [
+        'biome check --write --no-errors-on-unmatched  --files-ignore-unknown=true  --error-on-warnings',
+        'npm run typecheck'
+    ]
+ }    
+}
