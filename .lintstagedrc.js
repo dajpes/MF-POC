8603a2afe950e54f2d5eb5c7d22f3fbbe3c8f07c
Damian Perez <dajpes@gmail.com>
2026-01-08 19:52:15 -0500

    MF-5: Removed unused console log and fixed argument for lintstage

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,3 @@
 export default {
-  '{host,fan-wallet}/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --no-errors-on-unmatched --files-ignore-unknown --error-on-warnings'],
+  '{host,fan-wallet}/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --no-errors-on-unmatched --files-ignore-unknown=true --error-on-warnings'],
 }
--- host/src/components/SellTickets/SellTickets.tsx
+++ host/src/components/SellTickets/SellTickets.tsx
@@ -1,63 +1,61 @@
-import FanWallet from 'FanWallet/FanWallet'
-import { useEffect, useState } from 'react'
-import useAuth from '../../hooks/useAuth.tsx'
-import PleaseSignIn from '../PleaseSignIn/PleaseSignIn.tsx'
-import TicketSelection from './TicketSelection.tsx'
-import styles from './selltickets.module.css'
+import FanWallet from "FanWallet/FanWallet";
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth.tsx";
+import PleaseSignIn from "../PleaseSignIn/PleaseSignIn.tsx";
+import TicketSelection from "./TicketSelection.tsx";
+import styles from "./selltickets.module.css";
 
 export default function SellTickets() {
-  const { isAuthenticated } = useAuth()
-  const [tickets, setTickets] = useState([])
-  const [ticketsSelected, setTicketsSelected] = useState<number[]>([])
-  const [displayPaymentMethod, setDisplayPaymentMethod] = useState(false)
+	const { isAuthenticated } = useAuth();
+	const [tickets, setTickets] = useState([]);
+	const [ticketsSelected, setTicketsSelected] = useState<number[]>([]);
+	const [displayPaymentMethod, setDisplayPaymentMethod] = useState(false);
 
-  const onTicketSelectedHandler = (id: number) => {
-    setTicketsSelected((prev) =>
-      prev.includes(id) ? prev.filter((x) => x !== id) : [...prev, id],
-    )
-    setDisplayPaymentMethod(false)
-  }
+	const onTicketSelectedHandler = (id: number) => {
+		setTicketsSelected((prev) =>
+			prev.includes(id) ? prev.filter((x) => x !== id) : [...prev, id],
+		);
+		setDisplayPaymentMethod(false);
+	};
 
-  useEffect(() => {
-    if (!isAuthenticated) return
+	useEffect(() => {
+		if (!isAuthenticated) return;
 
-    const getTickets = async () => {
-      const ticketsResponse = await fetch('api/tickets.json')
-      const tickets = await ticketsResponse.json()
-      if (!tickets) return
+		const getTickets = async () => {
+			const ticketsResponse = await fetch("api/tickets.json");
+			const tickets = await ticketsResponse.json();
+			if (!tickets) return;
 
-      setTickets(tickets)
-    }
+			setTickets(tickets);
+		};
 
-    getTickets()
-  }, [isAuthenticated])
+		getTickets();
+	}, [isAuthenticated]);
 
-  if (!isAuthenticated) {
-    return <PleaseSignIn />
-  }
+	if (!isAuthenticated) {
+		return <PleaseSignIn />;
+	}
 
-  if (!tickets.length) {
-    return <p className={styles.noTicketsfound}>No tickets found.</p>
-    console.log('🚀 ~ tickets:', tickets)
+	if (!tickets.length) {
+		return <p className={styles.noTicketsfound}>No tickets found.</p>;
+	}
 
-  }
-
-  return (
-    <div className={styles.wrapper}>
-      <TicketSelection
-        tickets={tickets}
-        ticketsSelected={ticketsSelected}
-        onTicketSelected={onTicketSelectedHandler}
-        onContinue={() => setDisplayPaymentMethod(true)}
-      />
-      {displayPaymentMethod && ticketsSelected.length > 0 && (
-        <div className={styles.paymentMethod}>
-          <FanWallet />
-        </div>
-      )}
-      <div className={styles.listingComplete}>
-        <p>Create Listing</p>
-      </div>
-    </div>
-  )
+	return (
+		<div className={styles.wrapper}>
+			<TicketSelection
+				tickets={tickets}
+				ticketsSelected={ticketsSelected}
+				onTicketSelected={onTicketSelectedHandler}
+				onContinue={() => setDisplayPaymentMethod(true)}
+			/>
+			{displayPaymentMethod && ticketsSelected.length > 0 && (
+				<div className={styles.paymentMethod}>
+					<FanWallet />
+				</div>
+			)}
+			<div className={styles.listingComplete}>
+				<p>Create Listing</p>
+			</div>
+		</div>
+	);
 }
