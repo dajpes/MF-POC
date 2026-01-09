cecc3504025d56c3610601f4ceed367f8f920da3
Damian Perez <dajpes@gmail.com>
2026-01-08 20:02:15 -0500

    MF-5: Added biome path to lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,4 @@
 export default {
-  '{host,fan-wallet}/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --no-errors-on-unmatched --files-ignore-unknown=true --error-on-warnings'],
+  'host/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --config-path=./host --no-errors-on-unmatched --files-ignore-unknown=true --error-on-warnings'],
+  'fan-wallet/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --config-path=./fan-wallet --no-errors-on-unmatched --files-ignore-unknown=true --error-on-warnings'],
 }
--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,35 +1,35 @@
-import federation from "@originjs/vite-plugin-federation";
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig, loadEnv } from "vite";
-import biomePlugin from "vite-plugin-biome";
+import federation from '@originjs/vite-plugin-federation'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite'
+import biomePlugin from 'vite-plugin-biome'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, process.cwd())
 
-	return {
-		plugins: [
-			react(),
-			biomePlugin({
-				mode: "check",
-				applyFixes: true,
-				biomeCommandBase: "npx biome",
-			}),
-			federation({
-				name: "host_app",
-				remotes: {
-					FanWallet:
-						env.VITE_FAN_WALLET_URL ||
-						"http://localhost:4173/assets/fanWallet.js",
-				},
-				shared: ["react"],
-			}),
-		],
-		css: {
-			transformer: "lightningcss",
-		},
-		build: {
-			cssMinify: "lightningcss",
-		},
-	};
-});
+  return {
+    plugins: [
+      react(),
+      biomePlugin({
+        mode: 'check',
+        applyFixes: true,
+        biomeCommandBase: 'npx biome',
+      }),
+      federation({
+        name: 'host_app',
+        remotes: {
+          FanWallet:
+            env.VITE_FAN_WALLET_URL ||
+            'http://localhost:4173/assets/fanWallet.js',
+        },
+        shared: ['react'],
+      }),
+    ],
+    css: {
+      transformer: 'lightningcss',
+    },
+    build: {
+      cssMinify: 'lightningcss',
+    },
+  }
+})
