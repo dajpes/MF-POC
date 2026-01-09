4e9acd9b3a074786927fd96365312ff4498a8620
Damian Perez <dajpes@gmail.com>
2026-01-08 20:11:49 -0500

    MF-5: test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 export default {
-  'host/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --config-path ./host/biome.json --no-errors-on-unmatched --files-ignore-unknown --error-on-warnings'],
-  'fan-wallet/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --config-path ./fan-wallet/biome.json --no-errors-on-unmatched --files-ignore-unknown --error-on-warnings'],
+  'host/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --config-path=host --no-errors-on-unmatched --files-ignore-unknown=true --error-on-warnings'],
+  'fan-wallet/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --config-path=fan-wallet --no-errors-on-unmatched --files-ignore-unknown=true --error-on-warnings'],
 }
