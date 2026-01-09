f0538a934b2f9c4c2138fc622059b5c0fa07adc9
Damian Perez <dajpes@gmail.com>
2026-01-08 19:32:26 -0500

    MF-5: Removed repeated code

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,3 @@
 export default {
-  'host/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --no-errors-on-unmatched --files-ignore-unknown --error-on-warnings'],
-  'fan-wallet/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --no-errors-on-unmatched --files-ignore-unknown --error-on-warnings'],
+  '{host,fan-wallet}/**/*.{js,jsx,ts,tsx}': ['npx biome check --write --no-errors-on-unmatched --files-ignore-unknown --error-on-warnings'],
 }
